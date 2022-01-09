a3bf62e30678279894ad279346ec7a166900e0ed
Alan62 <beaucheron.alan@free.fr>
2022-01-09 19:40:50 +0100

    ajout event modalClose + entries cross

--- modal.js
+++ modal.js
@@ -10,10 +10,15 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalCross = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+
+modalCross.addEventListener ("click", closeModal);
 
 // launch modal form
 function launchModal() {
