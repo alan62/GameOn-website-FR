0c260c04d7c384b060b63af38d0db13689cf9142
Alan62 <beaucheron.alan@free.fr>
2022-01-11 17:04:02 +0100

    ajouter regex + entries

--- modal.js
+++ modal.js
@@ -13,6 +13,21 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const modalCross = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
+// FORM Elements
+
+const firstElt = document.getElementById("first");
+const lastElt = document.getElementById("last");
+const emailElt = document.getElementById("email");
+const birthdateElt = document.getElementById("birthdate");
+const qtyElt = document.getElementById("quantity");
+const cityElt = document.querySelector("input[type=radio]");
+const conditionsElt = document.getElementById("checkbox1");
+
+// Regex formats
+const birthdateFormat = /^(19|20)\d{2}[-](0?[1-9]|1[012])[-](0[1-9]|[12]\d|3[01])$/;
+const mailFormat = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+const positiveIntegerFormat = /^\+?(0|[1-9]\d*)$/;
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
